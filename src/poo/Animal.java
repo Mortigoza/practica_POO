a5181bc51ee74de5d56099cb42c51d4ec6a5d5ae
Cecilia Diaz <ceci.diazortigoza@gmail.com>
2021-09-15 22:07:22 -0300

    Adicionando lo de hoy

---
+++ src/poo/Animal.java
@@ -0,0 +1,17 @@
+package src.poo;
+
+public class Animal {
+    public String breed;
+    public String color;
+    public String name;
+
+    public boolean move(int distanceX, int distanceY) {
+        return true;
+    }
+    public String sleep() {
+        return "Estoy durmiendo";
+    }
+    public String eat() {
+        return "Estoy comiendo";
+    }
+}
